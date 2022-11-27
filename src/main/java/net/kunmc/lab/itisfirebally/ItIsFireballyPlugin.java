a84a4b31788479bd3816bf1d10bda163d16dcee2
Maru32768 <maru32768@gmail.com>
2022-11-27 19:13:45 +0900

    init

--- src/main/java/net/kunmc/lab/itisfirebally/ItIsFireballyPlugin.java
+++ src/main/java/net/kunmc/lab/itisfirebally/ItIsFireballyPlugin.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.itisfirebally;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class ItIsFireballyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
     }
